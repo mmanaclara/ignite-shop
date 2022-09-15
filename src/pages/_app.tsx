aca12a43786891db06906d6111dc6303fb952515
Clara Moreira <anacacamm@gmail.com>
2022-09-15 16:06:46 -0300

    feat: create header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
 
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
+
 globalStyles()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
