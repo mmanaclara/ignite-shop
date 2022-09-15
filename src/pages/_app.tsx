2d102f8c983c6d2b36e23ec9f09a5e5cad94430d
Clara Moreira <anacacamm@gmail.com>
2022-09-15 15:51:29 -0300

    feat: create global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+globalStyles()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
