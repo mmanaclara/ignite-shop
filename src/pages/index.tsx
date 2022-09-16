bdfb49006cad97ee2d88f99ef0a173eb4ad73b11
Clara Moreira <anacacamm@gmail.com>
2022-09-16 10:14:38 -0300

    feat: create slider with keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image"
+import { useKeenSlider } from 'keen-slider/react'
+
+import 'keen-slider/keen-slider.min.css'
 import { styled } from "../styles"
+
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import camiseta1 from '../assets/1.png'
@@ -8,22 +12,47 @@ import camiseta3 from '../assets/3.png'
 import camiseta4 from '../assets/4.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
-          <strong>Camiseta X</strong>
+          <strong>Camiseta A</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
 
         <footer>
-          <strong>Camiseta X</strong>
+          <strong>Camiseta B</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta C</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta D</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', {
   display: 'flex',
-  gap: '3rem',
   width: '100%',
   maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
   marginLeft: 'auto',
